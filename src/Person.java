894e104ce2bc20d80000639b53027efcdf0c97cb
cyber-zaka <zakaria.elyaouti@frg.wwschool.de>
2025-11-19 09:00:44 +0100

    Methoden

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,54 @@
 public class Person {
     String name;
     String vorname;
+    Person Nachfolger;
+
+
+    Person(){
+
+    }
+
+
+
+    /**
+     * Setzt den Namen auf pn
+     * @param pn
+     *
+     */
     void setName(String pn){name= pn;}
+    /**
+     * Setzt den Vornamen auf pv
+     * @param pv
+     *
+     */
     void setVorname(String pv){vorname=pv;}
+    /**
+     * Setzt den VollenNamen auf pn und pv
+     * @param pn, pv
+     *
+     */
+    void setVN(String pn, String pv){name=pn; vorname=pv;}
+    /**
+     * Setzt den Nachfolger auf pPerson
+     * @param pPerson
+     *
+     */
+    void setNachfolger(Person pPerson){Nachfolger=pPerson;}
+
+    /**
+     * Gibt den Namen zurück
+     * @return
+     */
+    String getName(){return name;}
+
+    /**
+     * Gibt den Vornamen zurück
+     * @return
+     */
+    String getVorname(){return vorname;}
+    /**
+     * Gibt den Nachfolger zurück
+     * @return
+     */
+    Person getNachfolger(){return Nachfolger;}
 }
--- src/Warteschlange.java
+++ src/Warteschlange.java
@@ -1,10 +1,63 @@
 public class Warteschlange {
-    private Person p1;
-    private Person p2;
+    private Person erster;
+    private Person letzter;
+
     Warteschlange(){
-        p1 = new Person ();
-        p2 = new Person();
+        erster = null;
+
     }
 
+    /**
+     * Lässt das Objekt die Schlange verlassen
+     */
+    void verlassen(){
+
+    }
+
+    /**
+     * gibt den Ersten in der Schlange raus
+     * @return
+     */
+    Person getErstePersonInSchlange(){
+        return erster;
+    }
+
+    /**
+     * fügt eine Person der schlange hinzu
+     * @param pPerson
+     */
+    void hintenanstellen(Person pPerson){
+
+    }
+
+    /**
+     * überprüft ob die Schlange leer ist
+     * @return
+     */
+    boolean IstLeer(){
+        return true;
+    }
+
+    /**
+     * Speichert die Person im ersten Platz der schlange
+     * @param pP
+     */
+    void setErster(Person pP){
+        this.erster=pP;
+    }
+
+    /**
+     * Speichert die Person im letzten Platz der schlange
+     * @param pP
+     */
+    public void setLetzter(Person pP) {
+        this.letzter = pP;
+    }
+    Person getErster(){
+        return erster;
+    }
+    Person getLetzter(){
+        return letzter;
+    }
 
 }
